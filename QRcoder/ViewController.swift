20d6d790bf400c860a634aca1934584d3cfb9b70
higuchiryunosuke <higuchi@mavericks09.com>
2020-01-21 11:04:52 +0900

    Initial Commit

---
+++ QRcoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRcoder
+//
+//  Created by higuchi の iMac on 2020/01/21.
+//  Copyright © 2020 higuchi の iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
